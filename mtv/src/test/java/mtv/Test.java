9ab5891e2d2a51d20be794593276d727fce8eb29
MunthaVeeresh <152646210+MunthaVeeresh@users.noreply.github.com>
2023-12-02 00:01:49 +0530

    Add files via upload

---
+++ mtv/src/test/java/mtv/Test.java
@@ -0,0 +1,16 @@
+package mtv;
+
+public class Test {
+	public static void main(String[] args) {
+		int i =10;
+		Test t = new Test();
+		//t.add(12, 30);
+		System.out.println(t.add(20, 30));
+		
+	}
+	public int add(int a,int b) {
+		int c = a + b;
+		return c;
+	} 
+
+}
